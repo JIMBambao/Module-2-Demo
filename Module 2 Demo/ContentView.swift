ee1ac5a296ea54ece71bd66eba3c5c71da6e8601
Jerome Bambao <2193871@slu.edu.ph>
2020-09-30 20:10:08 -0700

    Initial Commit

---
+++ Module 2 Demo/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Module 2 Demo
+//
+//  Created by J Bee on 9/30/20.
+//  Copyright © 2020 J Bee. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
